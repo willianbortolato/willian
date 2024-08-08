709c0bf9897d8881ded14e0ff80fe1531244c7bb
willianbortolato <161750796+willianbortolato@users.noreply.github.com>
2024-08-08 14:46:14 -0300

    Add files via upload

---
+++ lista de frutas.py
@@ -0,0 +1,10 @@
+#frutas - lista
+frutas = ['maça', 'banana', 'uva ']
+
+#adicionando frutas
+frutas.append('morango')
+print(frutas)
+
+#removendo frutas
+frutas.remove('banana')
+print(frutas)
---
+++ lista de numeros.py
@@ -0,0 +1,25 @@
+#definindo as primeiras listas
+alunos = ["jose", "joao", "luiz"]
+notas = [8.5, 9.2, 6.7]
+print(alunos)
+print(notas)
+lista_vazia = []
+print(lista_vazia)
+
+#lista com itens de diferentes tipos
+lista_misturada = [12, 15.56, 'sorveteria', ['baunilha', 'chocolate']]
+print(lista_misturada)
+
+
+#acessando elementos da lista
+print(alunos[0])
+print(notas[2])
+print(lista_misturada[1], lista_misturada[3])
+
+#modificando elementos de uma lista
+print(notas)
+notas[2] = 7.7
+print(notas)
+
+
+
