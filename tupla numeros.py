e157de4c9cbe13a7a2856dc6e89ffd911d82f2d2
willianbortolato <161750796+willianbortolato@users.noreply.github.com>
2024-08-22 14:28:41 -0300

    Add files via upload

---
+++ tupla numeros.py
@@ -0,0 +1,30 @@
+numeros = (10,20, 30, 40, 50)
+if 30 in numeros:
+    print('numero encontrado')
+else:
+    print('numero não encontrado')
+
+print()
+print()
+print()
+tupla_100 = (1, 2, 3, 4, 100)
+if 100 in tupla_100:
+    print('100 está na tupla')
+else:
+    print('100 não está na tupla')
+print()
+print()
+print()
+tupla_nomes = ('Ana, Willian, Michael')
+if 'Ana' in tupla_nomes:
+    print('ana está na tupla')
+else:
+    print('ana não esta na tupla')
+print()
+print()
+print()
+tupla_numerica = (4, 50, 74, 3, 156)
+if tupla_numerica[0] > 10:
+    print('o numero é maior que 10')
+else:
+    print('o numero é menor que 10')
