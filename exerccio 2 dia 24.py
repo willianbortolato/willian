347c834586623ccc6b7e1f480143c4c6f34ee572
willianbortolato <161750796+willianbortolato@users.noreply.github.com>
2025-05-08 15:11:43 -0300

    exerccio 2 dia 24.py

--- exerccio 2 dia 24.py
+++ exerccio 2 dia 24.py
@@ -1,36 +1,39 @@
-#2 – Faça um código que receba dois números inteiros, utilize as quatro
-#operações básicas pra gerar resultados com esses dois números e mostre os
-#resultados. Cada operação deve utilizar uma função.
-num1 = int(input('digite um numero inteiro:'))
-num2 = int(input('digite um numero inteiro:'))
 
-def soma(num1,num2):
+def soma(num1, num2):
     return num1 + num2
 
-def subtrair(num1,num2):
+def subtrair(num1, num2):
     return num1 - num2
 
-def multiplicar(num1,num2):
+def multiplicar(num1, num2):
     return num1 * num2
 
-def dividir(num1,num2):
+def dividir(num1, num2):
+    if num2 == 0:
+        return "Erro! Divisão por zero não é pode."
     return num1 / num2
 
 total = 0
 
 while True:
-    operação = input("digite 'soma', 'subtrair', 'multiplicar', 'dividir' ou 'sair': ").lower()
-    if operação == 'sair':
+
+    operacao = input("Digite 'soma', 'subtrair', 'multiplicar', 'dividir' ou 'sair': ").lower()
+
+    if operacao == 'sair':
         break
-    elif operação in ['somar', 'subtrair', 'multiplicar', 'dividir']:
-        valor = float(input('digite o valor:'))
-        if operação == 'somar':
+    elif operacao in ['soma', 'subtrair', 'multiplicar', 'dividir']:
+        num1 = int(input('Digite o primeiro número inteiro: '))
+        num2 = int(input('Digite o segundo número inteiro: '))
+        if operacao == 'soma':
             total = soma(num1, num2)
-
-        elif: opração ==
-        else:
+        elif operacao == 'subtrair':
             total = subtrair(num1, num2)
+        elif operacao == 'multiplicar':
+            total = multiplicar(num1, num2)
+        elif operacao == 'dividir':
+            total = dividir(num1, num2)
+        print(f'Resultado da {operacao}: {total}')
     else:
-        print('operação invalida')
+        print('Operação inválida')
 
-print(f'total final: resultado{total:.2f}')
+print('Fim das operações.')
